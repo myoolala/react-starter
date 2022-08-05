84bc47615e3779313e99c18ddd71bb1169e3b8c6
Peter Grasso <petergrasso@peters-mbp.lan>
2022-08-05 11:38:28 -0400

    BREAKING CHANGE: Adds numerous changes the make the repo more starter friendly
    feat: Adds a local docker build option
    feat: Adds a new script to build a static version of the app
    feat: Adds a more production focus node based docker build
    feat: Adds an httpd docker file to allow serving the ui outside of the api container
    docs: Updates readme to account for the new docker build
    feat: Adds a base devops container for terragrunt and terraform work
    feat: Updates yarn to npm
    feat: Adds redis support for remote session storage
    feat: Adds env var to allow for a cleaner method of importing environment based state
    chore: Removes reference to the master branch

--- app/src/lib/passport.js
+++ app/src/lib/passport.js
@@ -1,3 +1,14 @@
+'use-strict';
+
+const { createClient } = require('redis'),
+ 	  session = require('express-session'),
+	  throwIfCantConnect = true,
+	  connectRedis = require('connect-redis'),
+	  container = require('./winston'),
+	  config = require('./../config/config');
+
+let redisClient, RedisStore;
+
 /**
  * @function initialize
  * @description Setup and configure passport
@@ -15,3 +26,28 @@ module.exports.initialize = function initialize(passport) {
 
 	// TODO: Load any strategies
 };
+
+module.exports.configureRedis = async () => {
+    redisClient = createClient({
+		url: 'redis://redis:6379',
+		legacyMode: true
+	});
+    
+    let prom = redisClient.connect();
+    if (!throwIfCantConnect) {
+        prom = prom.catch(err => {
+            console.error(err);
+        });
+    }
+
+    await prom;
+
+	RedisStore = connectRedis(session);
+}
+
+module.exports.getSessionStore = () => {
+    if (config.server.useRedis) {
+		return new RedisStore({client: redisClient });
+	}
+	return undefined;
+}
