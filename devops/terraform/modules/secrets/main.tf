8eae7469e7c9036b0a6f4fabfb8dfae5939c76c9
Peter Grasso <petermgrasso@gmail.com>
2022-11-08 21:19:56 -0500

    The secrets now give access to the root account

--- devops/terraform/modules/secrets/main.tf
+++ devops/terraform/modules/secrets/main.tf
@@ -2,9 +2,26 @@
 
 # }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_secretsmanager_secret" secret {
     count = length(var.secrets)
 
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "secretsmanager:*"
+                Effect = "Allow"
+                Principal = {
+                    AWS = "${data.aws_caller_identity.current.account_id}:root"
+                }
+                # Gross and misleading I know, but it has to be star
+                # As it is attached to a single secret and AWS needs a resource line
+                Resource = "*"
+            }
+        ]
+    })
     name = var.secrets[count.index].name
     kms_key_id = var.kms_key_id
     recovery_window_in_days = var.recovery_window
